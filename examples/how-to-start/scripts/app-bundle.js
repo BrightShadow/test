51cf9e88ce47f7efbe5142ee62b748d097250d00
Paweł Porombka <pporombka@gmail.com>
2017-02-20 20:36:09 +0100

    Examples How-To-Start project first version

---
+++ examples/how-to-start/scripts/app-bundle.js
@@ -0,0 +1,48 @@
+define('app',["require", "exports"], function (require, exports) {
+    "use strict";
+    var App = (function () {
+        function App() {
+            this.message = 'Hello World!';
+        }
+        return App;
+    }());
+    exports.App = App;
+});
+
+define('environment',["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = {
+        debug: true,
+        testing: true
+    };
+});
+
+define('main',["require", "exports", "./environment"], function (require, exports, environment_1) {
+    "use strict";
+    Promise.config({
+        warnings: {
+            wForgottenReturn: false
+        }
+    });
+    function configure(aurelia) {
+        aurelia.use
+            .standardConfiguration()
+            .feature('resources');
+        if (environment_1.default.debug) {
+            aurelia.use.developmentLogging();
+        }
+        aurelia.start().then(function () { return aurelia.setRoot(); });
+    }
+    exports.configure = configure;
+});
+
+define('resources/index',["require", "exports"], function (require, exports) {
+    "use strict";
+    function configure(config) {
+    }
+    exports.configure = configure;
+});
+
+define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <h1>${message}</h1>\n</template>\n"; });
+//# sourceMappingURL=app-bundle.js.map
---
+++ examples/how-to-start/src/app.ts
@@ -0,0 +1,13 @@
+import {OrgChartSvg} from "orgchart.svg";
+
+export class App {
+  message = 'Hello World!';
+  
+  constructor() {
+      let orgchart = new OrgChartSvg(null);
+    
+  }
+  
+}
+
+
